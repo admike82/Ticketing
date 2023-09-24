44d3c2d9fce496ab73faa37d78a433a989bf8301
admike82 <admike82@gmail.com>
2023-09-24 17:40:30 +0200

    fix author not null => nullable

--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -18,7 +18,7 @@ class Ticket
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, nullable: true)]
     private ?string $author = null;
 
     #[ORM\Column(type: Types::TEXT)]
