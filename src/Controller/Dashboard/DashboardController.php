5b1b3296255320a23bc8616d5aae3e57baac2529
admike82 <admike82@gmail.com>
2023-10-04 22:17:17 +0200

    Page d'accueil du dashboard.

--- src/Controller/Dashboard/DashboardController.php
+++ src/Controller/Dashboard/DashboardController.php
@@ -4,6 +4,8 @@ namespace App\Controller\Dashboard;
 
 use App\Entity\UserAccount;
 use App\Repository\ApplicationRepository;
+use App\Repository\LevelRepository;
+use App\Repository\StatusRepository;
 use App\Repository\TicketRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,10 +16,29 @@ class DashboardController extends AbstractController
 
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(#[CurrentUser] ?UserAccount $user, TicketRepository $ticketRepository, LevelRepository $levelRepo, StatusRepository $statusRepository, ApplicationRepository $applicationRepository): Response
     {
+        $levels = $levelRepo->findAll();
+        $statuses = $statusRepository->findAll();
+        if ($user === null) {
+            return $this->redirectToRoute('app_login');
+        }
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            $tickets = $ticketRepository->findAll();
+        } else {
+            $applications = $applicationRepository->findBy(['userAccount' => $user->getId()]);
+            $ticketsApp = [];
+            foreach ($applications as $application) {
+                $ticketsApp = array_merge($ticketsApp, $application->getTickets()->toArray());
+            }
+            $ticketsUser = $ticketRepository->findBy(['userAccount' => $user->getId()]);
+            $tickets = array_merge($ticketsApp, $ticketsUser);
+        }
         return $this->render('dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController'
+            'controller_name' => 'DashboardController',
+            'tickets' => $tickets,
+            'statuses' => $statuses,
+            'levels' => $levels
         ]);
     }
 }
--- src/Controller/Dashboard/TicketsController.php
+++ src/Controller/Dashboard/TicketsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Dashboard;
 
+use App\Entity\Level;
+use App\Entity\Status;
 use App\Entity\UserAccount;
 use App\Repository\TicketRepository;
 use App\Repository\ApplicationRepository;
@@ -22,11 +24,47 @@ class TicketsController extends AbstractController
             $tickets = $ticketRepository->findAll();
         } else {
             $applications = $applicationRepository->findBy(['userAccount' => $user->getId()]);
-            $ticketsApp = [];
-            foreach ($applications as $application) {
-                $ticketsApp = array_merge($ticketsApp, $application->getTickets()->toArray());
-            }
+
+            $ticketsApp = $ticketRepository->findBy(['application' => $applications]);
             $ticketsUser = $ticketRepository->findBy(['userAccount' => $user->getId()]);
+            $tickets = array_merge($ticketsApp, $ticketsUser);
+        }
+        return $this->render('dashboard/tickets/index.html.twig', [
+            'tickets' => $tickets
+        ]);
+    }
+
+    #[Route('/dashboard/tickets/statuses/{id}', name: 'app_dashboard_tickets_statuses')]
+    public function statuses(Status $status, #[CurrentUser] ?UserAccount $user, TicketRepository $ticketRepository, ApplicationRepository $applicationRepository)
+    {
+        if ($user === null) {
+            return $this->redirectToRoute('app_login');
+        }
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            $tickets = $ticketRepository->findBy(['status' => $status]);
+        } else {
+            $applications = $applicationRepository->findBy(['userAccount' => $user->getId()]);
+            $ticketsApp = $ticketRepository->findBy(['application' => $applications, 'status' => $status]);
+            $ticketsUser = $ticketRepository->findBy(['userAccount' => $user->getId(), 'status' => $status]);
+            $tickets = array_merge($ticketsApp, $ticketsUser);
+        }
+        return $this->render('dashboard/tickets/index.html.twig', [
+            'tickets' => $tickets
+        ]);
+    }
+
+    #[Route('/dashboard/tickets/levels/{id}', name: 'app_dashboard_tickets_levels')]
+    public function levels(Level $level, #[CurrentUser] ?UserAccount $user, TicketRepository $ticketRepository, ApplicationRepository $applicationRepository)
+    {
+        if ($user === null) {
+            return $this->redirectToRoute('app_login');
+        }
+        if (in_array('ROLE_ADMIN', $user->getRoles())) {
+            $tickets = $ticketRepository->findBy(['level' => $level, 'status' => [1, 2]]);
+        } else {
+            $applications = $applicationRepository->findBy(['userAccount' => $user->getId()]);
+            $ticketsApp = $ticketRepository->findBy(['application' => $applications, 'level' => $level, 'status' => [1, 2]]);
+            $ticketsUser = $ticketRepository->findBy(['userAccount' => $user->getId(), 'level' => $level, 'status' => [1, 2]]);
             $tickets = array_merge($ticketsApp, $ticketsUser);
         }
         return $this->render('dashboard/tickets/index.html.twig', [
